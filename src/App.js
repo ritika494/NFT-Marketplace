588ad018a0ab947089ba51d9187f1773890e7b16
Ritika Garg <59705577+ritika494@users.noreply.github.com>
2023-08-09 18:43:04 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,1 +1,21 @@
+import "./App.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./Pages/Home";
+import Collection from "./Pages/Collection";
+import Create from "./Pages/Create";
 
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/collections" exact component={Collection} />
+          <Route path="/create" exact component={Create} />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
